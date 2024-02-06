a53a147387e8350f5739b73e1096ffe9f5bfdd41
Katriona Goldmann <katrionagoldmann@gmail.com>
2024-02-06 14:14:52 +0000

    Tidy and document

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # Pi inferences
 
 This repository contains scripts to run inferences on embedded device. The scripts firstly perform insect detection on images, followed by species classification of the insect crops. 
+
+
+## Set-up on Pi
+
+### Install Python3.9
+
+```
+sudo apt-get update
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+````
+
+```
+wget https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tar.xz
+tar xf Python-3.9.0.tar.xz
+cd Python-3.9.0
+./configure --enable-optimizations --prefix=/usr
+make
+```
+
+```
+sudo make altinstall
+```
+
+```
+cd ..
+sudo rm -r Python-3.9.0
+rm Python-3.9.0.tar.xz
+. ~/.bashrc
+```
+
+### Create venv
+
+```
+python3.9 -m venv ./venv_3.9
+source venv_3.9/bin/activate
+```
+
+Update pip
+
+```
+/home/pi/Desktop/model_data_bookworm/venv_3.9/bin/python3.9 -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
--- test_models.py
+++ test_models.py
@@ -1,29 +1,26 @@
-import sys
-print(sys.path)
-
 # For object detection
 import datetime
 import numpy as np
 import os
 
 from PIL import Image
-# ~ import pandas as pd
 from tflite_support.task import core
 from tflite_support.task import processor
 from tflite_support.task import vision
-import cv2
 import numpy as np
 
-# For sepcies detection
+# For species detection
 import json
 import tensorflow as tf
 import pandas as pd
-# ~ from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+#########################################
+# Moth Detection
+#########################################
 
 model = './gbif_model_metadata.tflite'
 enable_edgetpu = False
 num_threads = 1
-
 
 base_options = core.BaseOptions(file_name=model, use_coral=enable_edgetpu, num_threads=num_threads)
 detection_options = processor.DetectionOptions(max_results=20, score_threshold=0.1)
@@ -70,11 +67,7 @@ output_directory = './cropped_common_species/'
 
 for image_path in images:
     print(image_dir + image_path)
-    image = cv2.imread(image_dir + image_path)
-    image = np.array(image, dtype=np.uint8)
-
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #image = Image.open(image_dir + image_path)
+    image = np.asarray(Image.open(image_dir + image_path))
 
     # Create a TensorImage object from the RGB image.
     input_tensor = vision.TensorImage.create_from_array(image)
@@ -105,173 +98,9 @@ for image_path in images:
         counter += 1
         Image.fromarray(cropped_image).save(save_path)
 
-# ~ # Object Detection
-# ~ interpreter = tf.lite.Interpreter(model_path='./gbif_model.tflite')
-# ~ interpreter.allocate_tensors()
-
-# ~ # Set up the functions
-# ~ def preprocess_image(image_path, input_size):
-    # ~ """Preprocess the input image to feed to the TFLite model"""
-    # ~ img = tf.io.read_file(image_path)
-    # ~ img = tf.io.decode_image(img, channels=3)
-    # ~ img = tf.image.convert_image_dtype(img, tf.uint8)
-    # ~ original_image = img
-    # ~ resized_img = tf.image.resize(img, input_size)
-    # ~ resized_img = resized_img[tf.newaxis, :]
-    # ~ resized_img = tf.cast(resized_img, dtype=tf.uint8)
-    # ~ return resized_img, original_image
-
-
-# ~ def detect_objects(interpreter, image, threshold):
-    # ~ """Returns a list of detection results, each a dictionary of object info."""
-
-    # ~ signature_fn = interpreter.get_signature_runner()
-    # ~ print(signature_fn)
-
-    # ~ # Feed the input image to the model
-    # ~ output = signature_fn(images=image)
-
-    # ~ # Get all outputs from the model
-    # ~ count = int(np.squeeze(output['output_0']))
-    # ~ scores = np.squeeze(output['output_1'])
-    # ~ classes = np.squeeze(output['output_2'])
-    # ~ boxes = np.squeeze(output['output_3'])
-
-    # ~ results = []
-    # ~ for i in range(count):
-        # ~ if scores[i] >= threshold:
-            # ~ result = {
-            # ~ 'bounding_box': boxes[i],
-            # ~ 'class_id': classes[i],
-            # ~ 'score': scores[i]
-            # ~ }
-            # ~ results.append(result)
-    # ~ return results
-    
-# ~ def set_input_tensor(interpreter, image):
-  # ~ """Sets the input tensor."""
-  # ~ tensor_index = interpreter.get_input_details()[0]['index']
-  # ~ input_tensor = interpreter.tensor(tensor_index)()[0]
-  # ~ input_tensor[:, :] = image
-
-
-# ~ def get_output_tensor(interpreter, index):
-  # ~ """Returns the output tensor at the given index."""
-  # ~ output_details = interpreter.get_output_details()[index]
-  # ~ tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
-  # ~ return tensor
-  
-
-# ~ def detect_objects3(interpreter, image, threshold):
-  # ~ """Returns a list of detection results, each a dictionary of object info."""
-  # ~ output = interpreter.get_output_details()  # Model has single output.
-  # ~ input = interpreter.get_input_details()  # Model has single input.
-  # ~ #input_data = tf.constant(1., shape=[1, 1])
-  # ~ interpreter.set_tensor(input['index'], image)
-  # ~ interpreter.invoke()
-  # ~ print(interpreter.get_tensor(output['index']))
-
-  # ~ # Get all output details
-  # ~ boxes = get_output_tensor(interpreter, 0)
-  # ~ classes = get_output_tensor(interpreter, 1)
-  # ~ scores = get_output_tensor(interpreter, 2)
-  
-  # ~ print(boxes)
-  # ~ print(classes)
-  
-  # ~ #count = int(get_output_tensor(interpreter, 3))
-  
-
-
-  # ~ results = []
-  # ~ for i in range(1):
-    # ~ if scores[i] >= threshold:
-      # ~ result = {
-          # ~ 'bounding_box': boxes[i],
-          # ~ 'class_id': classes[i],
-          # ~ 'score': scores[i]
-      # ~ }
-      # ~ results.append(result)
-  # ~ return results
-    
-# ~ def detect_objects3(interpreter, image, threshold):
-    # ~ """Returns a list of detection results, each a dictionary of object info."""
-    
-    # ~ # Set input tensor
-    # ~ interpreter.set_tensor(interpreter.get_input_details()[0]['index'], image)
-
-    # ~ # Run inference
-    # ~ interpreter.invoke()
-
-    # ~ # Get output details
-    # ~ output_details = interpreter.get_output_details()
-    # ~ print(output_details)
-
-    # ~ # Extract output tensors
-    # ~ boxes = interpreter.get_tensor(output_details[0]['index'])
-    # ~ classes = interpreter.get_tensor(output_details[1]['index'])
-    # ~ scores = interpreter.get_tensor(output_details[2]['index'])
-    # ~ count = int(interpreter.get_tensor(output_details[3]['index'])[0])  # Extract the scalar value
-
-    # ~ results = []
-    # ~ for i in range(count):
-        # ~ if scores[0, i] >= threshold:
-            # ~ result = {
-                # ~ 'bounding_box': boxes[0, i],
-                # ~ 'class_id': classes[0, i],
-                # ~ 'score': scores[0, i]
-            # ~ }
-            # ~ results.append(result)
-    # ~ return results
-
-
-
-# ~ def run_odt_and_draw_results(image_path, interpreter, threshold=0.5, label=True, width=12):
-    # ~ """Run object detection on the input image and draw the detection results"""
-    # ~ # Load the input shape required by the model
-    # ~ _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
-    
-    # ~ basepath = image_path.split('/')[-1]
-
-    # ~ # Load the input image and preprocess it
-    # ~ preprocessed_image, original_image = preprocess_image(
-        # ~ image_path,
-        # ~ (input_height, input_width)
-    # ~ )
-
-    # ~ # Run object detection on the input image
-    # ~ results = detect_objects(interpreter, preprocessed_image, threshold=threshold)
-
-    # ~ # Plot the detection results on the input image
-    # ~ original_image_np = original_image.numpy().astype(np.uint8)
-    # ~ counter = 1
-    # ~ for obj in results:
-        # ~ # Convert the object bounding box from relative coordinates to absolute
-        # ~ # coordinates based on the original image resolution
-        # ~ ymin, xmin, ymax, xmax = obj['bounding_box']
-        # ~ xmin = int(xmin * original_image_np.shape[1])
-        # ~ xmax = int(xmax * original_image_np.shape[1])
-        # ~ ymin = int(ymin * original_image_np.shape[0])
-        # ~ ymax = int(ymax * original_image_np.shape[0])
-
-        # ~ # Find the class index of the current object
-        # ~ class_id = int(obj['class_id'])
-        # ~ class_str = ['moth', 'non_moth'][class_id]
-        
-        # ~ # crop the image to the bounding box
-        # ~ crop_img = original_image_np[ymin:ymax, xmin:xmax]
-        # ~ # save the image
-        # ~ Image.fromarray(crop_img).save('./cropped_common_species/' + class_str + '_' + str(counter) + '_' + basepath)
-        # ~ counter = counter + 1
-
-# ~ all_images = os.listdir('./common_species')
-# ~ for image in all_images:
-   # ~ image_file = './common_species/' + image
-   # ~ detection_result_image = run_odt_and_draw_results(
-       # ~ image_file,
-       # ~ interpreter,
-       # ~ threshold=0.3, width=2
-   # ~ )
+#########################################
+# Species Classification
+#########################################
 
 print('Now running species classification!!!')
 
@@ -329,7 +158,7 @@ for image_file in all_images:
     tflite_inf, conf, inf_time = tflite_inference(img, interpreter)
 
     file_path = file_path + [image_file]
-    truth = truth + [' '.join(image_file.split('/')[-1].split('_')[-3:-1])]
+    truth = truth + [' '.join(image_file.split('/')[-1].split('_')[0:2])]
     pred = pred + [species_names[tflite_inf]]
     confidence = confidence + [conf]
     time = time + [inf_time]
