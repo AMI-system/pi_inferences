59c2fd1e1ffabe8a421cc760eedc1ce821c8828d
KatrionaGoldmann <kgoldmann@turing.ac.uk>
2024-03-08 16:43:57 +0000

    working inference

--- desktop_inferences.py
+++ desktop_inferences.py
@@ -21,8 +21,6 @@ def preprocess_image(image_path, input_size):
 def detect_objects(interpreter, image, threshold):
     """Detects objects in the input image using the provided interpreter."""
     signature_fn = interpreter.get_signature_runner()
-    print(signature_fn)
-
 
     # Feed the input image to the model
     output = signature_fn(images=image)
@@ -33,16 +31,36 @@ def detect_objects(interpreter, image, threshold):
     classes = np.squeeze(output['output_2'])
     boxes = np.squeeze(output['output_3'])
 
-    results = []
+    detections_list = []
+
     for i in range(count):
-        if scores[i] >= threshold:
-            result = {
-                'bounding_box': boxes[i],
-                'class_id': classes[i],
-                'score': scores[i]
+        bounding_box = boxes[i]
+
+        origin_y, origin_x, max_y, max_x = bounding_box[0], bounding_box[1], bounding_box[2], bounding_box[3]
+        width, height = max_x-origin_x, max_y-origin_y
+
+        if scores[i] > threshold:
+            category_dict = {
+                'index': i,
+                'score': scores[i],
+                'display_name': classes[i],
+                'category_name': class_labels[int(classes[i])],
             }
-            results.append(result)
-    return results
+
+            detection_dict = {
+                'counter': i,
+                'bounding_box': {
+                    'origin_x': origin_x,
+                    'origin_y': origin_y,
+                    'width': width,
+                    'height': height,
+                },
+                'categories': [category_dict],
+            }
+
+            detections_list.append(detection_dict)
+
+    return detections_list
 
 def run_moth_detection(image_path, m_interpreter, threshold=0.5):
     """Runs object detection on the input image and returns the detection results."""
@@ -56,9 +74,12 @@ def run_moth_detection(image_path, m_interpreter, threshold=0.5):
     )
 
     # Run object detection on the input image
+    a = datetime.datetime.now()
     results = detect_objects(m_interpreter, preprocessed_image, threshold=threshold)
+    b = datetime.datetime.now()
+    c = b - a
 
-    return results, original_image
+    return results, original_image, str(c.microseconds)
 
 def species_inference(crop_image, species_interpreter):
     """Performs species classification on a cropped image."""
@@ -81,7 +102,7 @@ def species_inference(crop_image, species_interpreter):
 nclass = 2
 DETECTION_THRESHOLD = 0.1
 label_map = {1: 'moth', 2: 'nonmoth'}
-classes = ['moth', 'nonmoth']
+class_labels = ['moth', 'nonmoth']
 region = 'uk'
 
 # Load moth detection model
@@ -101,42 +122,28 @@ annotated_image_path = os.path.join('./annotated_images/',
                                         os.path.basename(image_path))
 
 # Run moth detection
-detections_list, original_image = run_moth_detection(image_path, moth_interpreter, threshold=DETECTION_THRESHOLD)
+detections_list, original_image, det_time = run_moth_detection(image_path, moth_interpreter, threshold=DETECTION_THRESHOLD)
+original_image_np = original_image.numpy().astype(np.uint8)
 
 # Process each detected moth
 for detection in detections_list:
     bounding_box = detection['bounding_box']
-    print(bounding_box)
-    origin_x, origin_y, max_x, max_y = bounding_box[0], bounding_box[1], bounding_box[2], bounding_box[3]
-    width = max_x - origin_x
-    height = max_y - origin_y
-    print(origin_x, origin_y, width, height)
 
+    origin_x, origin_y, width, height = bounding_box['origin_x'], bounding_box['origin_y'], bounding_box['width'], bounding_box['height']
 
-    # Ensure indices are within bounds and cast to integers if necessary
-    # origin_y = int(origin_y)
-    # origin_x = int(origin_x)
-    # height = int(height)
-    # width = int(width)
-
-
-    print("Bounding Box Coordinates:")
-    print("origin_y:", origin_y)
-    print("origin_x:", origin_x)
-    print("height:", height)
-    print("width:", width)
+    # convert these to pixels
+    xmin = int(origin_x * original_image_np.shape[1])
+    xmax = int((origin_x + width) * original_image_np.shape[1])
+    ymin = int(origin_y * original_image_np.shape[0])
+    ymax = int((origin_y + height) * original_image_np.shape[0])
 
     # Slice the image using integer indices
-    cropped_image = image[origin_y:origin_y + height, origin_x:origin_x + width]
-    #category_name = detection['categories'][0]['category_name']
-    category_name = 'moth'
-    #insect_score = detection['categories'][0]['score']
+    cropped_image = image[ymin:ymax, xmin:xmax]
+    category_name = detection['categories'][0]['category_name']
+    insect_score = detection['categories'][0]['score']
     resized_image = Image.fromarray(cropped_image).convert("RGB").resize((300, 300))
     img = np.array(resized_image) / 255.0
     img = (img - 0.5) / 0.5
-
-    ## Crop the image to the bounding box
-    #cropped_image = image[origin_y:origin_y + height, origin_x:origin_x + width]
 
     # Perform species classification
     species_inf, conf, inf_time = species_inference(crop_image=img, species_interpreter=species_interpreter)
@@ -145,22 +152,22 @@ for detection in detections_list:
     bbox_color = (46, 139, 87) if category_name == 'moth' else (238, 75, 43)
     ann_label = f"{species_names[species_inf]}, {conf:.2f}"
     cv2.rectangle(annot_image,
-                    (origin_x, origin_y),
-                    (origin_x + width, origin_y + height),
+                    (xmin, ymin),
+                    (xmax, ymax),
                     bbox_color, 4)
-    # cv2.putText(annot_image, text=ann_label,
-    #             org=(origin_x, max_y),
-    #             fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-    #             fontScale=1.2, color=bbox_color, thickness=4)
+    cv2.putText(annot_image, text=ann_label,
+                org=(xmin, ymin),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1.2, color=bbox_color, thickness=4)
 
     # Save inference results to csv
     df = pd.DataFrame({
         'image_path': [image_path],
         'timestamp': [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
         'moth_class': [category_name],
-        #'insect_score': [insect_score],
-        #'detection_time': [str(c.microseconds)],
-        #'bounding_box': ['; '.join(map(str, bounding_box.values()))],
+        'insect_score': [insect_score],
+        'detection_time': [det_time],
+        'bounding_box': ['; '.join(map(str, [xmin, ymin, xmax, ymax]))],
         'annot_path': [annotated_image_path],
         'species_inference_time': [inf_time],
         'truth': [' '.join(image_path.split('/')[-1].split('_')[0:2])],
@@ -171,5 +178,5 @@ for detection in detections_list:
     df['correct'] = np.where(df['pred'] == df['truth'], 1, 0)
     df.to_csv(f'./results/{region}_predictions.csv', index=False, mode='a', header=False)
 
-print(annotated_image_path)
+print('Saved annotated image to: ', annotated_image_path)
 cv2.imwrite(annotated_image_path, cv2.cvtColor(annot_image, cv2.COLOR_BGR2RGB))
